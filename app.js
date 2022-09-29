8909c800c5e7e8cd0509dcc90e222f96d347f0ae
Kiprotich Michael <kiprotichmicheal570@gmail.com>
2022-09-30 01:54:56 +0300

    added introduction section

---
+++ app.js
@@ -0,0 +1,27 @@
+footer {
+    background: var(--footer-bg);
+}
+
+footer .logo {
+    max-width: 100px;
+}
+
+footer {
+    color: #fff;
+}
+
+footer .subscription a,
+footer .sitemap a {
+    display: block;
+    color: #ccc;
+    text-decoration: none;
+    font-size: 12px;
+    transition: all 0.2s ease;
+}
+
+footer .subscription a:hover,
+footer .sitemap a:hover {
+    text-decoration: underline;
+    color: #fff;
+}
+
